5496962ecc1fa1b08baa81637694bfdab1841c1c
Dessy524 <debrahdesmond9@gmail.com>
2026-07-16 19:25:03 +0000

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,22 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.android.application")
+    // Add the Google services plugin
+    id("com.google.gms.google-services") 
 }
 
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+
+    // Add the dependencies for the Firebase products you want to use
+    // When using the BoM, you do not specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+}
 android {
     namespace = "com.example.campus_eats"
     compileSdk = flutter.compileSdkVersion
